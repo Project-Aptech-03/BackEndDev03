ea31ca1e871d4a4eeba2c4d6c9cefa30d90fe1fa
Datgau <Datbeo1204@gmail.com>
2025-09-23 17:57:39 +0700

    chore(authservice): verify register

--- ProjectDemoWebApi/Controllers/OrdersController.cs
+++ ProjectDemoWebApi/Controllers/OrdersController.cs
@@ -24,7 +24,6 @@ namespace ProjectDemoWebApi.Controllers
                 ?? throw new UnauthorizedAccessException("User not authenticated");
         }
 
-        // Top 3 sản phẩm bán chạy nhất
         [HttpGet("top-products")]
         public async Task<IActionResult> GetTop3Products(CancellationToken cancellationToken)
         {
